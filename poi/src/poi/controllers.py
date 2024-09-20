79252d5e23ed23f9468106c4d663ff5e4431f239
Ahmad Idris Hammaadama <ahmadidris67@gmail.com>
2024-09-20 10:53:40 +0100

    modified poi

--- poi/src/poi/controllers.py
+++ poi/src/poi/controllers.py
@@ -1,5 +1,6 @@
 from flask import jsonify, request, g, json
 from datetime import datetime as dt
+from datetime import datetime
 from .models import Poi
 from .. import db
 from ..util import save_audit_data, custom_jwt_required
@@ -199,44 +200,134 @@ def get_poi(poi_id):
 def update_poi(poi_id):
     data = request.json
     poi = Poi.query.get(poi_id)
-    if poi:
-        poi.update(
-            first_name=data.get('first_name'),
-            last_name=data.get('last_name'),
-            ref_numb=data.get('ref_numb'),
-            dob=data.get('dob'),
-            passport_number=data.get('passport_number'),
-            other_id_number=data.get('other_id_number'),
-            phone_number=data.get('phone_number'),
-            email=data.get('email'),
-            role=data.get('role'),
-            affiliation=data.get('affiliation'),
-            address=data.get('address'),
-            remark=data.get('remark'),
-            middle_name=data.get('middle_name'),
-            alias=data.get('alias'),
-            picture=data.get('picture'),
-            crime_committed=data.get('crime_committed'),
-            crime_date=data.get('crime_date'),
-            casualties_recorded=data.get('casualties_recorded'),
-            arresting_body=data.get('arresting_body'),
-            place_of_detention=data.get('place_of_detention'),
-            action_taken=data.get('action_taken'),
-            category_id=data.get('category_id'),
-            source_id=data.get('source_id'),
-            country_id=data.get('country_id'),
-            state_id=data.get('state_id'),
-            gender_id=data.get('gender_id'),
-            deleted_at=data.get('deleted_at')
-        )
-        return jsonify(poi.to_dict()), 200
-    return jsonify({'message': 'POI not found'}), 404
+
+    try:
+        response = {}
+        if poi:
+            poi.update(
+                first_name=data.get('first_name'),
+                last_name=data.get('last_name'),
+                ref_numb=data.get('ref_numb'),
+                dob=data.get('dob'),
+                passport_number=data.get('passport_number'),
+                other_id_number=data.get('other_id_number'),
+                phone_number=data.get('phone_number'),
+                email=data.get('email'),
+                role=data.get('role'),
+                affiliation=data.get('affiliation'),
+                address=data.get('address'),
+                remark=data.get('remark'),
+                middle_name=data.get('middle_name'),
+                alias=data.get('alias'),
+                picture=data.get('picture'),
+                crime_committed=data.get('crime_committed'),
+                crime_date=data.get('crime_date'),
+                casualties_recorded=data.get('casualties_recorded'),
+                arresting_body=data.get('arresting_body'),
+                place_of_detention=data.get('place_of_detention'),
+                action_taken=data.get('action_taken'),
+                category_id=data.get('category_id'),
+                source_id=data.get('source_id'),
+                country_id=data.get('country_id'),
+                state_id=data.get('state_id'),
+                gender_id=data.get('gender_id'),
+                deleted_at=data.get('deleted_at')
+            )
+
+            current_time = dt.utcnow()
+            audit_data = {
+                "user_id": g.user["id"] if hasattr(g, "user") else None,
+                "first_name": g.user["first_name"] if hasattr(g, "user") else None,
+                "last_name": g.user["last_name"] if hasattr(g, "user") else None,
+                "pfs_num": g.user["pfs_num"] if hasattr(g, "user") else None,
+                "user_email": g.user["email"] if hasattr(g, "user") else None,
+                "event": "update_poi",
+                "auditable_id": poi.id,
+                "old_values": None,
+                "new_values": json.dumps({
+                    "ref_numb": poi.ref_numb,
+                    "first_name": poi.first_name,
+                    "middle_name": poi.middle_name,
+                    "last_name": poi.last_name,
+                    "alias": poi.alias,
+                    "dob": poi.dob,
+                    "passport_number": poi.passport_number,
+                    "other_id_number": poi.other_id_number,
+                    "phone_number": poi.phone_number,
+                    "email": poi.email,
+                    "role": poi.role,
+                    "affiliation_id": poi.affiliation_id,
+                    "crime_committed": poi.crime_committed,
+                    "crime_date": poi.crime_date,
+                    "casualties_recorded": poi.casualties_recorded,
+                    "arresting_body": poi.arresting_body,
+                    "place_of_detention": poi.place_of_detention,
+                    "action_taken": poi.action_taken,
+                    "address": poi.address,
+                    "remark": poi.remark,
+                    "category_id": poi.category_id,
+                    "source_id": poi.source_id,
+                    "country_id": poi.country_id,
+                    "state_id": poi.state_id,
+                    "gender_id": poi.gender_id,
+                    "deleted_at": poi.deleted_at, 
+                }),
+                "url": request.url,
+                "ip_address": request.remote_addr,
+                "user_agent": request.user_agent.string,
+                "tags": "POI, Update",
+                "created_at": current_time.isoformat(),
+                "updated_at": current_time.isoformat(),
+            }
+
+            save_audit_data(audit_data)
+
+            response["status"] = "success"
+            response["status_code"] = 201
+            response["message"] = "POI updated successfully"
+    except KeyError as e:
+        return jsonify({"error": f"Missing field: {str(e)}"}), 400
+    except Exception as e:
+        db.session.rollback()
+        response["status"] = "error"
+        response["status_code"] = 500
+        response["message"] = f"An error occurred while update the poi: {str(e)}"
+
+    return jsonify(response), response["status_code"]
 
 # Soft Delete POI
 @custom_jwt_required
 def delete_poi(poi_id):
     poi = Poi.query.get(poi_id)
     if poi:
+        current_time = datetime.utcnow()
+        audit_data = {
+            "user_id": g.user["id"] if hasattr(g, "user") else None,
+            "first_name": g.user["first_name"] if hasattr(g, "user") else None,
+            "last_name": g.user["last_name"] if hasattr(g, "user") else None,
+            "pfs_num": g.user["pfs_num"] if hasattr(g, "user") else None,
+            "user_email": g.user["email"] if hasattr(g, "user") else None,
+            "event": "delete_poi",
+            "auditable_id": poi.id,
+            "old_values": None,
+            "new_values": json.dumps(
+                {
+                    "first_name": poi.first_name,
+                    "middle_name": poi.middle_name,
+                    "last_name": poi.last_name,
+                    "alis": poi.alias
+                }
+            ),
+            "url": request.url,
+            "ip_address": request.remote_addr,
+            "user_agent": request.user_agent.string,
+            "tags": "POI, Delete",
+            "created_at": current_time.isoformat(),
+            "updated_at": current_time.isoformat(),
+        }
+
+        save_audit_data(audit_data)
+
         poi.soft_delete()
         db.session.commit()
         return jsonify({'message': 'POI deleted successfully'}), 200
@@ -246,11 +337,51 @@ def delete_poi(poi_id):
 @custom_jwt_required
 def restore_poi(poi_id):
     poi = Poi.query.filter(Poi.id == poi_id, Poi.deleted_at != None).first()
-    if poi:
+
+    if poi is None:
+        return jsonify({"message": "POI not found"}), 404
+
+    try:
+        # Audit - Record before restoration
+        current_time = datetime.utcnow()
+        audit_data = {
+            "user_id": g.user["id"] if hasattr(g, "user") else None,
+            "first_name": g.user["first_name"] if hasattr(g, "user") else None,
+            "last_name": g.user["last_name"] if hasattr(g, "user") else None,
+            "pfs_num": g.user["pfs_num"] if hasattr(g, "user") else None,
+            "user_email": g.user["email"] if hasattr(g, "user") else None,
+            "event": "restore_poi",
+            "auditable_id": poi.id,
+            "old_values": None,
+            "new_values": json.dumps(
+                {
+                    "first_name": poi.first_name,
+                    "middle_name": poi.middle_name,
+                    "last_name": poi.last_name,
+                    "alis": poi.alias
+                }
+            ),
+            "url": request.url,
+            "ip_address": request.remote_addr,
+            "user_agent": request.user_agent.string,
+            "tags": "POI, Restore",
+            "created_at": current_time.isoformat(),
+            "updated_at": current_time.isoformat(),
+        }
+
+        save_audit_data(audit_data)
+
         poi.restore()
         db.session.commit()
-        return jsonify(poi.to_dict()), 200
-    return jsonify({'message': 'POI not found or not deleted'}), 404
+        return (
+            jsonify({"message": "POI restored successfully"}),
+            200,
+        )
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"message": "Error restoring POI", "error": str(e)}), 500
+    finally:
+        db.session.close()
 
 # List all POIs
 @custom_jwt_required
@@ -291,6 +422,28 @@ def list_pois():
 
     # Format response
     pois_list = [poi.to_dict() for poi in paginated_pois.items]
+
+    current_time = datetime.utcnow()
+    audit_data = {
+            "user_id": g.user["id"] if hasattr(g, "user") else None,
+            "first_name": g.user["first_name"] if hasattr(g, "user") else None,
+            "last_name": g.user["last_name"] if hasattr(g, "user") else None,
+            "pfs_num": g.user["pfs_num"] if hasattr(g, "user") else None,
+            "user_email": g.user["email"] if hasattr(g, "user") else None,
+            "event": "list_poi",
+            "auditable_id": None,
+            "old_values": None,
+            "new_values": None,
+            "url": request.url,
+            "ip_address": request.remote_addr,
+            "user_agent": request.user_agent.string,
+            "tags": "POI, List",
+            "created_at": current_time.isoformat(),
+            "updated_at": current_time.isoformat(),
+        }
+
+    save_audit_data(audit_data)
+    
     return jsonify({
         'total': paginated_pois.total,
         'pages': paginated_pois.pages,
