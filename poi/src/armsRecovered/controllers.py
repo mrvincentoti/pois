5eee87212cb60f95a8b1eb591447af990db2c5e0
Ahmad Idris Hammaadama <ahmadidris67@gmail.com>
2024-09-22 14:43:55 +0100

    added arms recovery module

--- poi/src/armsRecovered/controllers.py
+++ poi/src/armsRecovered/controllers.py
@@ -1,18 +1,20 @@
-from flask import request, jsonify,json, g
+from flask import request, jsonify, json, g
 from sqlalchemy import or_
 from sqlalchemy.exc import SQLAlchemyError
 from datetime import datetime
 from .. import db
 from .models import ArmsRecovered
+from ..arms.models import Arms
+from ..poi.models import Poi
 from ..util import custom_jwt_required, save_audit_data
 
 def slugify(text):
     return text.replace(' ', '-').lower()
 
 @custom_jwt_required
-def get_arms():
+def get_arms_recovered():
     try:
-        arms = Arm.query.all()
+        arms = ArmsRecovered.query.all()
 
         arm_list = []
         for arm in arms:
@@ -29,20 +31,27 @@ def get_arms():
 
 
 @custom_jwt_required
-def add_arm():
+def add_arm_recovered():
     if request.method == "POST":
         data = request.get_json()
-        arm_name = data.get("name")
+        arm_id = data.get("arm_id")
+        poi_id = data.get("poi_id")
+        location = data.get("location")
+        comments = data.get("comments")
+        recovery_date = data.get("recovery_date")
+        created_by = g.user["id"]
 
-        if not arm_name:
-            return jsonify({"message": "Name is required"}), 400
-
-        new_arm = Arm(
-            name=arm_name
+        new_recovered_arm = ArmsRecovered(
+            arm_id=arm_id,
+            poi_id=poi_id,
+            location=location,
+            comments=comments,
+            recovery_date=recovery_date,
+            created_by=created_by
         )
 
         try:
-            db.session.add(new_arm)
+            db.session.add(new_recovered_arm)
             db.session.commit()
             
             current_time = datetime.utcnow()
@@ -52,39 +61,69 @@ def add_arm():
                 "last_name": g.user["last_name"] if hasattr(g, "user") else None,
                 "pfs_num": g.user["pfs_num"] if hasattr(g, "user") else None,
                 "user_email": g.user["email"] if hasattr(g, "user") else None,
-                "event":"add_arm",
-                "auditable_id": new_arm.id,
+                "event":"add_recovered_arm",
+                "auditable_id": new_recovered_arm.id,
                 "old_values": None,
                 "new_values": json.dumps(
                     {
-                        "arm_name": arm_name
+                        "arm_id": arm_id,
+                        "poi_id":poi_id,
+                        "location":location,
+                        "comments":comments,
+                        "recovery_date":recovery_date,
+                        "created_by":created_by
                     }
                 ),
                 "url": request.url,
                 "ip_address": request.remote_addr,
                 "user_agent": request.user_agent.string,
-                "tags": "Auth, Arm, Create",
+                "tags": "ArmsRecovered, Create",
                 "created_at": current_time.isoformat(),
                 "updated_at": current_time.isoformat(),
             }
 
             save_audit_data(audit_data)
             
-            return jsonify({"message": "Arm added successfully"}), 201
+            return jsonify({"message": "Recovered arm added successfully"}), 201
         except Exception as e:
             db.session.rollback()
-            return jsonify({"message": "Error adding arm", "error": str(e)}), 500
+            return jsonify({"message": "Error adding arm recovered", "error": str(e)}), 500
         finally:
             db.session.close()
 
 
 @custom_jwt_required
-def get_arm(arm_id):
-    arm = Arm.query.filter_by(id=arm_id, deleted_at=None).first()
-    if arm:
+def get_arm_recovered(recovery_id):
+    arm = ArmsRecovered.query.filter_by(id=recovery_id, deleted_at=None).first()
+    arm_recovery = ArmsRecovered.query.filter_by(id=recovery_id, deleted_at=None).first()
+    
+    if arm_recovery:
+        # Fetch the associated arm name
+        arm = Arms.query.filter_by(id=arm_recovery.arm_id, deleted_at=None).first()
+        arm_name = arm.name if arm else "Unknown Arm"
+
+        # Fetch the associated POI details
+        poi = Poi.query.filter_by(id=arm_recovery.poi_id, deleted_at=None).first()
+        if poi:
+            poi_name = f"{poi.first_name} {poi.middle_name or ''} {poi.last_name} ({poi.alias or ''})".strip()
+        else:
+            poi_name = "Unknown POI"
+
+        # Fetch the name of the user who created the record
+        created_by = User.query.filter_by(id=arm_recovery.created_by, deleted_at=None).first()
+        created_by_name = f"{created_by.username} ({created_by.email})" if created_by else "Unknown User"
+
+        # Prepare the response data with the fetched names
         arm_data = {
-            "id": arm.id,
-            "name": arm.name
+            "arm_id": arm_recovery.id,
+            "arm_name": arm_name,
+            "poi_id": arm.poi_id,
+            "poi_name": poi_name,
+            "location": arm_recovery.location,
+            "comments": arm_recovery.comments,
+            "recovery_date": arm_recovery.recovery_date,
+            "created_by_id": arm.created_by,
+            "created_by": created_by_name
         }
 
         current_time = datetime.utcnow()
@@ -94,14 +133,14 @@ def get_arm(arm_id):
             "last_name": g.user["last_name"] if hasattr(g, "user") else None,
             "pfs_num": g.user["pfs_num"] if hasattr(g, "user") else None,
             "user_email": g.user["email"] if hasattr(g, "user") else None,
-            "event": "get_arm",
+            "event": "get_recovered_arm",
             "auditable_id": None,
             "old_values": None,
             "new_values": None,
             "url": request.url,
             "ip_address": request.remote_addr,
             "user_agent": request.user_agent.string,
-            "tags": "Auth, Arm, Get",
+            "tags": "ArmsRecovered, Get",
             "created_at": current_time.isoformat(),
             "updated_at": current_time.isoformat(),
         }
@@ -110,15 +149,15 @@ def get_arm(arm_id):
 
         return jsonify({"arm": arm_data})
     else:
-        return jsonify({"message": "Arm not found"}), 404
+        return jsonify({"message": "Arm Recovered not found"}), 404
 
 
 @custom_jwt_required
-def edit_arm(arm_id):
-    arm = Arm.query.filter_by(id=arm_id, deleted_at=None).first()
+def edit_arm_recovered(recovery_id):
+    arm = ArmsRecovered.query.filter_by(id=recovery_id, deleted_at=None).first()
 
     if arm is None:
-        return jsonify({"message": "Arm not found"}), 404
+        return jsonify({"message": "ArmsRecovered not found"}), 404
 
     data = request.get_json()
     arm_name = data.get("name")
@@ -138,7 +177,7 @@ def edit_arm(arm_id):
                 "last_name": g.user["last_name"] if hasattr(g, "user") else None,
                 "pfs_num": g.user["pfs_num"] if hasattr(g, "user") else None,
                 "user_email": g.user["email"] if hasattr(g, "user") else None,
-                "event":"edit_arm",
+                "event":"edit_recovered_arm",
                 "auditable_id": arm.id,
                 "old_values": None,
                 "new_values": json.dumps(
@@ -149,14 +188,14 @@ def edit_arm(arm_id):
                 "url": request.url,
                 "ip_address": request.remote_addr,
                 "user_agent": request.user_agent.string,
-                "tags": "Auth, Arm, Update",
+                "tags": "Auth, ArmsRecovered, Update",
                 "created_at": current_time.isoformat(),
                 "updated_at": current_time.isoformat(),
         }
 
         save_audit_data(audit_data)
         
-        return jsonify({"message": "Arm updated successfully"}), 200
+        return jsonify({"message": "ArmsRecovered updated successfully"}), 200
     except Exception as e:
         db.session.rollback()
         return jsonify({"message": "Error updating arm", "error": str(e)}), 500
@@ -165,11 +204,11 @@ def edit_arm(arm_id):
 
 
 @custom_jwt_required
-def delete_arm(arm_id):
-    arm = Arm.query.filter_by(id=arm_id, deleted_at=None).first()
+def delete_arm_recovered(recovery_id):
+    arm = ArmsRecovered.query.filter_by(id=recovery_id, deleted_at=None).first()
 
     if arm is None:
-        return jsonify({"message": "Arm not found"}), 404
+        return jsonify({"message": "ArmsRecovered not found"}), 404
 
     try:
         arm.soft_delete()
@@ -182,7 +221,7 @@ def delete_arm(arm_id):
                 "last_name": g.user["last_name"] if hasattr(g, "user") else None,
                 "pfs_num": g.user["pfs_num"] if hasattr(g, "user") else None,
                 "user_email": g.user["email"] if hasattr(g, "user") else None,
-                "event":"delete_arm",
+                "event":"delete_recovered_arm",
                 "auditable_id": arm.id,
                 "old_values": None,
                 "new_values": json.dumps(
@@ -193,14 +232,14 @@ def delete_arm(arm_id):
                 "url": request.url,
                 "ip_address": request.remote_addr,
                 "user_agent": request.user_agent.string,
-                "tags": "Auth, Arm, Delete",
+                "tags": "Auth, ArmsRecovered, Delete",
                 "created_at": current_time.isoformat(),
                 "updated_at": current_time.isoformat(),
         }
 
         save_audit_data(audit_data)
         
-        return jsonify({"message": "Arm deleted successfully"}), 200
+        return jsonify({"message": "ArmsRecovered deleted successfully"}), 200
     except Exception as e:
         db.session.rollback()
         return jsonify({"message": "Error deleting arm", "error": str(e)}), 500
@@ -209,11 +248,11 @@ def delete_arm(arm_id):
 
 
 @custom_jwt_required
-def restore_arm(arm_id):
-    arm = Arm.query.filter_by(id=arm_id).first()
+def restore_arm_recovered(recovery_id):
+    arm = ArmsRecovered.query.filter_by(id=recovery_id).first()
 
     if arm is None:
-        return jsonify({"message": "Arm not found"}), 404
+        return jsonify({"message": "ArmsRecovered not found"}), 404
 
     try:
         # Audit - Record before restoration
@@ -224,7 +263,7 @@ def restore_arm(arm_id):
             "last_name": g.user["last_name"] if hasattr(g, "user") else None,
             "pfs_num": g.user["pfs_num"] if hasattr(g, "user") else None,
             "user_email": g.user["email"] if hasattr(g, "user") else None,
-            "event": "restore_arm",
+            "event": "restore_recovered_arm",
             "auditable_id": arm.id,
             "old_values": None,
             "new_values": json.dumps(
@@ -235,7 +274,7 @@ def restore_arm(arm_id):
             "url": request.url,
             "ip_address": request.remote_addr,
             "user_agent": request.user_agent.string,
-            "tags": "Setup, Arm, Restore",
+            "tags": "Setup, ArmsRecovered, Restore",
             "created_at": current_time.isoformat(),
             "updated_at": current_time.isoformat(),
         }
@@ -245,7 +284,7 @@ def restore_arm(arm_id):
         arm.restore()
         db.session.commit()
         return (
-            jsonify({"message": "Arm restored successfully"}),
+            jsonify({"message": "ArmsRecovered restored successfully"}),
             200,
         )
     except Exception as e:
--- poi/src/armsRecovered/urls.py
+++ poi/src/armsRecovered/urls.py
@@ -1,11 +1,23 @@
 from flask import request
 
 from ..app import app
-from .controllers import get_arms_recovered
+from .controllers import add_arm_recovered, get_arms_recovered, get_arm_recovered, edit_arm_recovered, delete_arm_recovered, restore_arm_recovered
 from .models import ArmsRecovered
 
-
-@app.route("/recoveries", methods=['GET', 'POST'])
+@app.route("/recovered-arms", methods=['GET', 'POST'])
 def list_arms_recovered():
    if request.method == 'GET': return get_arms_recovered()
-   return 'Method is Not Allowed'
+   if request.method == 'POST': return add_arm_recovered()
+   else: return 'Method is Not Allowed'
+
+@app.route("/recovered-arms/<recovery_id>", methods=['GET', 'PUT', 'DELETE'])
+def retrieve_update_destroy_arms_recovered(recovery_id):
+   if request.method == 'GET': return get_arm_recovered(recovery_id)
+   if request.method == 'PUT': return edit_arm_recovered(recovery_id)
+   if request.method == 'DELETE': return delete_arm_recovered(recovery_id)
+   else: return 'Method is Not Allowed'
+
+@app.route("/recovered-arm/restore/<recovery_id>", methods=['GET'])
+def restore_single_arm_recovered(recovery_id):
+   if request.method == 'GET': return restore_arm_recovered(recovery_id)
+   else: return 'Method is Not Allowed'
