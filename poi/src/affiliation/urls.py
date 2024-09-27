e5000572fdc0baf851f38571983a85cd143ef45d
Ahmad Idris Hammaadama <ahmadidris67@gmail.com>
2024-09-27 15:33:02 +0100

    completed affiliations module

--- poi/src/affiliation/urls.py
+++ poi/src/affiliation/urls.py
@@ -1,11 +1,24 @@
 from flask import request
 
 from ..app import app
-from .controllers import get_affiliations
+from .controllers import add_affiliation, get_affiliations, get_affiliation, edit_affiliation, delete_affiliation, restore_affiliation
 from .models import Affiliation
 
 
 @app.route("/affiliations", methods=['GET', 'POST'])
 def list_affiliations():
    if request.method == 'GET': return get_affiliations()
-   return 'Method is Not Allowed'
+   if request.method == 'POST': return add_affiliation()
+   else: return 'Method is Not Allowed'
+
+@app.route("/affiliations/<affiliation_id>", methods=['GET', 'PUT', 'DELETE'])
+def retrieve_update_destroy_affiliations(affiliation_id):
+   if request.method == 'GET': return get_affiliation(affiliation_id)
+   if request.method == 'PUT': return edit_affiliation(affiliation_id)
+   if request.method == 'DELETE': return delete_affiliation(affiliation_id)
+   else: return 'Method is Not Allowed'
+
+@app.route("/affiliation/restore/<affiliation_id>", methods=['GET'])
+def restore_single_affiliation(affiliation_id):
+   if request.method == 'GET': return restore_affiliation(affiliation_id)
+   else: return 'Method is Not Allowed'
