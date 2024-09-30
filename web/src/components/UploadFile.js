577d2113ba0ad239f5e67ad4791988c55e348a33
Muhammad Amir Abdulkadir <ameerdinakj@gmail.com>
2024-09-30 13:40:22 +0100

    edit poi

--- web/src/components/UploadFile.js
+++ web/src/components/UploadFile.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
 import { message, Upload } from 'antd';
 
-const UploadFilePicture = ({ imageUrl, changeImage }) => {
+const UploadFilePicture = ({ imageString, imageUrl, changeImage }) => {
 	const [loading, setLoading] = useState(false);
 	const [imgUrl, setImgUrl] = useState();
+	const [imgString, setImgString] = useState();
 
 	const getBase64 = (img, callback) => {
 		const reader = new FileReader();
@@ -20,22 +21,34 @@ const UploadFilePicture = ({ imageUrl, changeImage }) => {
 		const isLt2M = file.size / 1024 / 1024 < 2;
 		if (!isLt2M) {
 			message.error('Image must smaller than 2MB!');
+			return false;
 		}
 
-		setImgUrl(file);
-		changeImage(file);
+		const fileData = {
+			originFileObj: file, // Store the original file object
+			url: URL.createObjectURL(file), // Create a temporary URL for preview
+		};
+
+		changeImage(fileData); // Pass the formatted object
+
+		getBase64(file, url => {
+			setImgUrl(url);
+		});
 
 		return false;
 	};
 
 	useEffect(() => {
-		if (imageUrl) {
-			setImgUrl(imageUrl);
+		if (imageString) {
+			setImgUrl(imageString);
 		}
-	}, [imageUrl]);
+	}, [imageUrl, imageString]);
 
 	const handleChange = info => {
-		if (info.file.status === 'done' && info.file.originFileObj) {
+		changeImage(info);
+
+		if (info.file.status === 'done' || info.file.originFileObj) {
+			// Convert file to base64
 			setLoading(true);
 			getBase64(info.file.originFileObj, url => {
 				setImgUrl(url);
