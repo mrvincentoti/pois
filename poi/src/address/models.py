67e8123a9b6c7d717a57c87172802747f673cda9
Ahmad Idris Hammaadama <ahmadidris67@gmail.com>
2024-10-03 09:54:21 +0100

    added address model

--- poi/src/address/models.py
+++ poi/src/address/models.py
@@ -1,12 +1,18 @@
 from datetime import datetime
 from .. import db # from __init__.py
 
-class Directorate(db.Model):
-    __tablename__ = 'directorate'
+class Address(db.Model):
+    __tablename__ = 'addresses'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(16))
-    description = db.Column(db.String(32))
-    deleted_at = db.Column(db.DateTime, nullable=True)
+    poi_id = db.Column(db.Integer, db.ForeignKey('poi.id'))
+    address_type = db.Column(db.String(64))
+    address = db.Column(db.Text, nullable=True)
+    latitude = db.Column(db.String(64), nullable=True)
+    longitude = db.Column(db.String(64), nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    
+    poi = db.relationship('Poi', backref='poi', lazy=True)
     
     def soft_delete(self):
         self.deleted_at = datetime.now()
