d530ce3adb76ffef95c857f5a174f3771cb5b8dd
Ahmad Idris Hammaadama <ahmadidris67@gmail.com>
2024-10-16 12:41:46 +0100

    modified category

--- poi/src/category/models.py
+++ poi/src/category/models.py
@@ -10,20 +10,12 @@ class Category(db.Model):
     deleted_at = db.Column(db.DateTime, nullable=True)
 
     def to_dict(self):
-        # Format the category type as required
-        formatted_type = None
-        if self.category_type == "org":
-            formatted_type = "Organisation"
-        elif self.category_type == "poi":
-            formatted_type = "POI"
-        else:
-            formatted_type = "Null"
-
         return {
-            "id": self.id,
-            "name": self.name,
-            "description": self.description,
-            "category_type": formatted_type  # Use the formatted category type
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'category_type': self.category_type,
+            'deleted_at': self.deleted_at
         }
 
     def __init__(self, name, description, category_type):
