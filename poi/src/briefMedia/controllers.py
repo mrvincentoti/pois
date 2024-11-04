1904d628f1b3f833c582d5973d5c2672138c6633
Ahmad Idris Hammaadama <ahmadidris67@gmail.com>
2024-11-04 10:54:25 +0100

    fixes

--- poi/src/briefMedia/controllers.py
+++ poi/src/briefMedia/controllers.py
@@ -5,8 +5,8 @@ from .models import BriefMedia
 from ..brief.models import Brief
 from datetime import datetime
 from dotenv import load_dotenv
-from ..util import custom_jwt_required, save_audit_data, upload_file_to_minio,get_media_type_from_extension, allowed_file
-from flask import jsonify, request, g, json, current_app
+from ..util import custom_jwt_required, save_audit_data, upload_file_to_minio,get_media_type_from_extension, allowed_file, minio_client
+from flask import jsonify, request, g, json
 from werkzeug.utils import secure_filename
 from urllib.parse import urljoin
 
